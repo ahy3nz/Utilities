9ff03d65bb6d3361dda4911907220b92ff82b9ea
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-01-23 13:33:41 -0600

    indentation

--- massMove.py
+++ massMove.py
@@ -19,15 +19,15 @@ for i, val in enumerate(filenames):
         print('*********************************')
         print('{:^20s}'.format(val))
         print('*********************************')
-	if options.toRahman:
-		os.system('scp -r /global/cscratch1/sd/ahy3nz/Trajectories/{} $RAHMAN:Trajectories/'.format(val,val))
-	if options.toFiley:
-		os.system('scp -r /global/cscratch1/sd/ahy3nz/Trajectories/{} $FILEY:Trajectories/'.format(val,val))
-	if options.fromRahman:
-		os.system('scp -r $RAHMAN:Trajectories/{} /global/cscratch1/sd/ahy3nz/Trajectories'.format(val))
-		#os.system('scp -r $RAHMAN:Programs/setup/Bilayer/{} /global/cscratch1/sd/ahy3nz/Trajectories'.format(val))
-	else:
-		pass
+        if options.toRahman:
+	    	os.system('scp -r /global/cscratch1/sd/ahy3nz/Trajectories/{} $RAHMAN:Trajectories/'.format(val,val))
+	    if options.toFiley:
+	    	os.system('scp -r /global/cscratch1/sd/ahy3nz/Trajectories/{} $FILEY:Trajectories/'.format(val,val))
+	    if options.fromRahman:
+	    	os.system('scp -r $RAHMAN:Trajectories/{} /global/cscratch1/sd/ahy3nz/Trajectories'.format(val))
+	    	#os.system('scp -r $RAHMAN:Programs/setup/Bilayer/{} /global/cscratch1/sd/ahy3nz/Trajectories'.format(val))
+	    else:
+	    	pass
 
 
 
