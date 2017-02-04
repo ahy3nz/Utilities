470d049b8d6f91e6d182400c2b49f7b06097defa
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-02-03 18:43:39 -0600

    added accre stuff

--- massMove.py
+++ massMove.py
@@ -9,6 +9,8 @@ parser.add_option("-f", action="store", type="string", default = "somebilayer", 
 parser.add_option("--toRAHMAN", action="store_true", dest = "toRahman")
 parser.add_option("--toFILEY", action="store_true", dest="toFiley")
 parser.add_option("--fromRAHMAN", action="store_true", dest = "fromRahman")
+parser.add_option("--toACCRE", action="store_true", dest = "toACCRE")
+
 (options, args) = parser.parse_args()
 
 inputfile = open(options.filename,'r')
@@ -25,6 +27,9 @@ for i, val in enumerate(filenames):
 	    elif options.toFiley:
 	    	os.system('scp -r {}/{} $FILEY:Trajectories/'.format(os.getcwd,val))
             os.system('scp -r {}/{} $FILEY:Trajectories/'.format(os.getcwd, inputfile))
+        elif options.toACCRE:
+            os.system('scp -r {}/{} $ACCRE:Trajectories'.format(os.getcwd, val))
+            os.system('scp -r {}/{} $ACCRE:Trajectories'.format(os.getcwd, inputfile))
 	    elif options.fromRahman:
 	    	os.system('scp -r $RAHMAN:Trajectories/{} {}'.format(val.os.getcwd))
 	    	#os.system('scp -r $RAHMAN:Programs/setup/Bilayer/{} /global/cscratch1/sd/ahy3nz/Trajectories'.format(val))
