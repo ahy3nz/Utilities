839e02b9104a2293797c40d6e5040c0e3428147d
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-03-06 11:39:18 -0600

    Fixed up the masmove

--- massMove.py
+++ massMove.py
@@ -14,8 +14,10 @@ parser.add_option("--toTITAN", action="store_true", dest = "toTITAN")
 
 (options, args) = parser.parse_args()
 
-inputfile = open(options.filename,'r')
-filenames = inputfile.readlines()
+#inputfile = open(options.filename,'r')
+#filenames = inputfile.readlines()
+with open(options.filename) as f:
+    filenames = list(f)
 for i, val in enumerate(filenames):
     if "#" not in val and val.rstrip():
         val = val.strip()
@@ -23,22 +25,22 @@ for i, val in enumerate(filenames):
         print('{:^20s}'.format(val))
         print('*********************************')
         if options.toRahman:
-	    	os.system('scp -r {}/{} $RAHMAN:Trajectories/'.format(os.getcwd(), val))
-            os.system('scp -r {}/{} $RAHMAN:Trajectories/'.format(os.getcwd(), inputfile))
-	    elif options.toFiley:
-	    	os.system('scp -r {}/{} $FILEY:Trajectories/'.format(os.getcwd,val))
-            os.system('scp -r {}/{} $FILEY:Trajectories/'.format(os.getcwd, inputfile))
+            os.system('scp -r {}/{} $RAHMAN:Trajectories/'.format(os.getcwd(), val))
+            os.system('scp  {}/{} $RAHMAN:Trajectories/'.format(os.getcwd(), options.filename))
+        elif options.toFiley:
+            os.system('scp -r {}/{} $FILEY:Trajectories/'.format(os.getcwd(),val))
+            os.system('scp  {}/{} $FILEY:Trajectories/'.format(os.getcwd(), options.filename))
         elif options.toACCRE:
-            os.system('scp -r {}/{} $ACCRE:Trajectories'.format(os.getcwd, val))
-            os.system('scp -r {}/{} $ACCRE:Trajectories'.format(os.getcwd, inputfile))
+            os.system('scp -r {}/{} $ACCRE:Trajectories'.format(os.getcwd(), val))
+            os.system('scp  {}/{} $ACCRE:Trajectories'.format(os.getcwd(), options.filename))
         elif options.toTITAN:
-            os.system('scp -r {}/{} $TITAN:/lustre/atlas/scratch/ahy3nz/mat149/Trajectories'.format(os.getcwd, val))
-            os.system('scp -r {}/{} $TITAN:/lustre/atlas/scratch/ahy3nz/mat149/Trajectories'.format(os.getcwd, inputfile))
-	    elif options.fromRahman:
-	    	os.system('scp -r $RAHMAN:Trajectories/{} {}'.format(val.os.getcwd))
+            os.system('scp -r {}/{} $TITAN:/lustre/atlas/scratch/ahy3nz/mat149/Trajectories'.format(os.getcwd(), val))
+            os.system('scp  {}/{} $TITAN:/lustre/atlas/scratch/ahy3nz/mat149/Trajectories'.format(os.getcwd(), inputfile))
+        elif options.fromRahman:
+            os.system('scp -r $RAHMAN:Trajectories/{} {}'.format(val,os.getcwd))
 	    	#os.system('scp -r $RAHMAN:Programs/setup/Bilayer/{} /global/cscratch1/sd/ahy3nz/Trajectories'.format(val))
-	    else:
-	    	pass
+        else:
+            pass
 
 
 
