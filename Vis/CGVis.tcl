26011c51b1950e43c635dc7895158bd5ba093ada
Alex Yang <ahy3nz@filey.vuse.vanderbilt.edu>
2017-03-07 13:52:26 -0600

    CG visualization

--- Vis/CGVis.tcl
+++ Vis/CGVis.tcl
@@ -1,17 +1,17 @@
 # VMD for MACOSXX86, version 1.9.1 (February 4, 2012)
 # Log file '/Users/ahy3nz/Programs/setup/CoarseGraining/CG_Vis', created by user ahy3nz
-
+source cg_bonds.tcl
 
 mol modselect 0 0 resid 1 to 64
-mol modstyle 0 0 VDW 1.000000 12.000000
+mol modstyle 0 0 Licorice 1.000000 15.000000 12.000000
 mol color Name
 mol representation VDW 1.000000 12.000000
 mol selection resid 1 to 64
 
 mol addrep 0
 mol modselect 1 0 resid 65 to 128
+mol modstyle 1 0 Licorice 1.000000 15.000000 12.000000
 mol color Name
-mol representation VDW 1.000000 12.000000
 mol selection resid 65 to 128
 
 mol addrep 0
