3ed0a8879305847f23b1db0c439a40845ea25f88
Alex Yang <ahy3nz@filey.vuse.vanderbilt.edu>
2017-03-08 16:52:03 -0600

    Change texture of CG vis

--- Vis/CGVis.tcl
+++ Vis/CGVis.tcl
@@ -1,6 +1,6 @@
 # VMD for MACOSXX86, version 1.9.1 (February 4, 2012)
 # Log file '/Users/ahy3nz/Programs/setup/CoarseGraining/CG_Vis', created by user ahy3nz
-source cg_bonds.tcl
+source ~/Programs/Utilities/Vis/cg_bonds.tcl
 
 mol modselect 0 0 resid 1 to 64
 mol modstyle 0 0 Licorice 1.000000 15.000000 12.000000
@@ -17,7 +17,7 @@ mol selection resid 65 to 128
 mol addrep 0
 mol modselect 2 0 resid 129 to 10000
 mol modcolor 2 0 ColorID 1
-mol modmaterial 2 0 Transparent
+mol modmaterial 2 0 Glass2
 mol color ColorID 1
 mol representation VDW 1.000000 12.000000
 #mol selection resid 129 to 768
