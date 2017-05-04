a75c6190f9f445d8ab7d40bfa8490258b887ec67
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-05-04 16:19:14 -0500

    add edison moving

--- newmassMove.py
+++ newmassMove.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from optparse import OptionParser
 """
 Move things to rahman, from rahman, or to filey based on folder name in the input file
@@ -12,6 +13,7 @@ parser.add_option("--toFILEY", action="store_true", dest="toFiley")
 parser.add_option("--fromRAHMAN", action="store_true", dest = "fromRahman")
 parser.add_option("--toACCRE", action="store_true", dest = "toACCRE")
 parser.add_option("--toTITAN", action="store_true", dest = "toTITAN")
+parser.add_option("--toEDISON", action="store_true", dest = "toEDISON")
 
 (options, args) = parser.parse_args()
 
@@ -30,6 +32,8 @@ elif options.toACCRE:
     os.system('scp -r {}/{} $ACCRE:Trajectories'.format(os.getcwd(), tar_ball))
 elif options.toTITAN:
     os.system('scp -r {}/{} $TITAN:/lustre/atlas/scratch/ahy3nz/mat149/Trajectories'.format(os.getcwd(), tar_ball))
+elif options.toEDISON:
+    subprocess.Popen('scp -r {}/{} $EDISON:/scratch2/scratchdirs/ahy3nz/Trajectories'.format(os.getcwd(), tar_ball), shell= True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 elif options.fromRahman:
     os.system('scp -r $RAHMAN:Trajectories/{} {}'.format(val,tar_ball))
 	#os.system('scp -r $RAHMAN:Programs/setup/Bilayer/{} /global/cscratch1/sd/ahy3nz/Trajectories'.format(val))
