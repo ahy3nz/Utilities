24a82bf76e0a5f9c43543a1e25c44ba75e643fb2
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-05-10 10:45:00 -0500

    Fix subprocess popen calls

--- newmassMove.py
+++ newmassMove.py
@@ -19,6 +19,8 @@ parser.add_option("--toEDISON", action="store_true", dest = "toEDISON")
 
 with open(options.filename) as f:
     filenames = list(f)
+#options.filename = '90_WVTR_4-29b'
+#filenames = ['90_WVTR_4-29b']
 # Compress everything before sending, folders and filename
 to_compress = ' '.join([line.rstrip() for line in filenames])
 tar_ball = options.filename[:-4]+'.tar'
@@ -33,7 +35,9 @@ elif options.toACCRE:
 elif options.toTITAN:
     os.system('scp -r {}/{} $TITAN:/lustre/atlas/scratch/ahy3nz/mat149/Trajectories'.format(os.getcwd(), tar_ball))
 elif options.toEDISON:
-    subprocess.Popen('scp -r {}/{} $EDISON:/scratch2/scratchdirs/ahy3nz/Trajectories'.format(os.getcwd(), tar_ball), shell= True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p=subprocess.Popen('scp -r {}/{} $EDISON:/scratch2/scratchdirs/ahy3nz/Trajectories'.format(os.getcwd(), tar_ball), shell= True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p.wait()
+    print(p.stdout.read())
 elif options.fromRahman:
     os.system('scp -r $RAHMAN:Trajectories/{} {}'.format(val,tar_ball))
 	#os.system('scp -r $RAHMAN:Programs/setup/Bilayer/{} /global/cscratch1/sd/ahy3nz/Trajectories'.format(val))
