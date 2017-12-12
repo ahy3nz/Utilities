fa17a3db36636f538fc71f7455888ab07f929c9c
Alex Yang <ahy3nz@virginia.edu>
2017-12-12 11:09:26 -0600

    Explicitly state each keyword for visualiztion

--- visualize_msibi.py
+++ visualize_msibi.py
@@ -1,6 +1,7 @@
 from msibi_utils.plot_fit import plot_all_fits
 from msibi_utils.animate_rdf import animate_all_pairs_states
 
-logfile = '12-4.log'
+logfile = 'from_accre/12-4.log'
 plot_all_fits(logfile,use_agg=True,ylims=(-1,1))  
-animate_all_pairs_states(logfile,"./rdfs", use_agg=True)
+animate_all_pairs_states(logfile,"from_accre/rdfs", potentials_dir="from_accre/potentials",
+        rdf_dir="from_accre/rdfs", use_agg=True)
