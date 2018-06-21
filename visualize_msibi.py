ba5c8b3d83b86262fdf6cae8d350d1d8e6b7d84f
Alex Yang <ayang41@DeltaDurian.local>
2018-06-21 10:44:34 -0500

    Modify default msibi visualziation script to include identifying best fits

--- visualize_msibi.py
+++ visualize_msibi.py
@@ -1,7 +1,12 @@
 from msibi_utils.plot_fit import plot_all_fits
 from msibi_utils.animate_rdf import animate_all_pairs_states
+from msibi_utils.parse_logfile import parse_logfile
+from msibi_utils.find_best_iterations import find_best_iterations
 
 logfile = 'charmm_bi/6-12-18.log'
+log_info = parse_logfile(logfile)
+best_iterations = find_best_iterations(logfile)
+print(best_iterations)
 plot_all_fits(logfile,use_agg=True,ylims=(0,1))  
 animate_all_pairs_states(logfile,"charmm_bi/rdfs", 
         potentials_dir="charmm_bi/potentials",
