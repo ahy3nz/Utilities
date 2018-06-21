b7a61fe195306c864fb31f9d4dfe11516cf70a7d
Alex Yang <ayang41@DeltaDurian.local>
2018-06-21 10:04:19 -0500

    Update default msibi visualization for plotting two optimizations

--- visualize_msibi.py
+++ visualize_msibi.py
@@ -1,7 +1,10 @@
 from msibi_utils.plot_fit import plot_all_fits
 from msibi_utils.animate_rdf import animate_all_pairs_states
 
-logfile = 'from_accre/12-4.log'
-plot_all_fits(logfile,use_agg=True,ylims=(-1,1))  
-animate_all_pairs_states(logfile,"from_accre/rdfs", potentials_dir="from_accre/potentials",
-        rdf_dir="from_accre/rdfs", use_agg=True)
+logfile = 'charmm_bi/6-12-18.log'
+plot_all_fits(logfile,use_agg=True,ylims=(0,1))  
+animate_all_pairs_states(logfile,"charmm_bi/rdfs", 
+        potentials_dir="charmm_bi/potentials",
+        rdf_dir="charmm_bi/rdfs", 
+        potentials2_dir="charmm_bulk_debi_flhe_dspc/potentials",
+        rdf2_dir="charmm_bulk_debi_flhe_dspc/rdfs", use_agg=True)
