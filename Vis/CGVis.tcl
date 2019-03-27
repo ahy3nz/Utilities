893a6253813855be5fe2b00e041d9238c14a8154
Alex Yang <ahy3nz@virginia.edu>
2019-03-27 11:56:07 -0500

    Some cg visualization script

---
+++ Vis/CGVis.tcl
@@ -0,0 +1,14 @@
+# VMD for MACOSXX86, version 1.9.3 (November 30, 2016)
+mol delrep 0 0
+mol addrep 0
+mol modselect 0 0 {not name W}
+mol modstyle 0 0 Licorice 0.100000 12.000000 12.000000
+mol modmaterial 0 AOChalky
+
+mol addrep 0
+mol modselect 1 0 {name W}
+mol modcolor 1 0 ColorID 23
+mol modstyle 1 0 Licorice 0.100000 12.000000 12.000000
+mol modmaterial 1 0 AOChalky
+rotate x by -90
+axes location off
